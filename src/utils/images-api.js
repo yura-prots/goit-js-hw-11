b4d2e0b40ea8cbfbe35937f556e3c7cb0a73cdd4
yura-prots <yuraprots@gmail.com>
2023-12-30 18:05:02 +0200

    fixed request

--- src/utils/images-api.js
+++ src/utils/images-api.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41545079-e9b3a4168a776ff0916b92321';
 
-export async function getImages() {
+export async function getImages(userSearch) {
   try {
-    const response = await axios.get(`${BASE_URL}/?key=${API_KEY}`);
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${userSearch}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
     console.log(response);
 
     return response;
