53f16c34beda671fc40a1c8b106330e1a1ebfe8c
yura-prots <yuraprots@gmail.com>
2023-12-31 16:35:59 +0200

    add Notiflix

--- src/index.js
+++ src/index.js
@@ -12,16 +12,29 @@ function onFormSubmit(e) {
   const request = e.target.elements.searchQuery.value;
 
   createGallery(request);
+
+  refs.searchForm.reset();
 }
 
 async function createGallery(userQuery) {
   try {
     const response = await getImages(userQuery);
-    console.log(response);
     const imagesArr = response.data.hits;
 
+    if (!imagesArr.length) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+
+      return;
+    }
+
     createMarkup(imagesArr);
+
+    Notiflix.Notify.success(`Hooray! We found ${response.data.total} images.`);
   } catch (error) {
     console.log(error);
+
+    Notiflix.Notify.failure('Ooops, something went wrong!');
   }
 }
--- src/utils/images-api.js
+++ src/utils/images-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/apiiji/';
 const API_KEY = '41545079-e9b3a4168a776ff0916b92321';
 
 export async function getImages(query) {
